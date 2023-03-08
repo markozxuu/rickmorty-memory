26ba8603e159fb5b6312645482fada645c8b4d40
Markoz Pena <markdrew53@gmail.com>
2023-03-08 13:53:26 -0600

    feat: 🎸 create "<Header />" component

---
+++ src/components/commons/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from '../../../assets/images/ricky_morty_logo.svg';
+import './header.scss';
+
+const Header = () => (
+  <header className="container">
+    <img src={logo} className="logo" alt="logo de rick and mort" />
+    <h1 className="label">Juego de memoria</h1>
+  </header>
+);
+
+export default Header;
