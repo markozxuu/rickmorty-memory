a46f08bba1ae021133222ebe160029133f8d1560
Markoz Pena <markdrew53@gmail.com>
2023-03-09 06:03:30 -0600

    feat: 🎸 impring the quality code

--- src/pages/Game/components/CongratsMessage/index.tsx
+++ src/pages/Game/components/CongratsMessage/index.tsx
@@ -1,18 +1,24 @@
+import Link from '@/components/ui/Link';
+
+import './congratsMessage.scss';
+
 interface CongratsMessageProps {
   turns: number;
+  resetGame: () => void;
 }
 
-const CongratsMessage = ({ turns }: CongratsMessageProps) => {
-  console.log(turns);
+const CongratsMessage = ({ turns, resetGame }: CongratsMessageProps) => {
   return (
-    <section
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        background: 'blue',
-      }}
-    >
-      <h1>felcidades</h1>
+    <section className="congrats-content">
+      <h1 className="congrats-title">¡Felicitaciones!</h1>
+      <p className="congrats-text">Terminastes el juego con {turns} turnos</p>
+
+      <div className="root-buttons">
+        <button className="btn-reset" onClick={resetGame}>
+          Repetir
+        </button>
+        <Link type="secondary" path="/" label="inicio" />
+      </div>
     </section>
   );
 };
