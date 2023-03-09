8fd43caf9c868b761bcf63c1c22fdd2591caf6b1
Markoz Pena <markdrew53@gmail.com>
2023-03-08 21:23:52 -0600

    feat: 🎸 improving the styles

--- src/components/commons/Header/index.tsx
+++ src/components/commons/Header/index.tsx
@@ -4,8 +4,8 @@ import logo from '@/assets/images/ricky_morty_logo.svg';
 import './header.scss';
 
 const Header = () => (
-  <header className="container">
-    <img src={logo} className="logo" alt="logo de rick and mort" />
+  <header className="containerr">
+    <img src={logo} className="logo-app" alt="logo de rick and mort" />
     <h1 className="label">Juego de memoria</h1>
   </header>
 );
