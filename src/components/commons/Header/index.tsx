0dc003bf0e390e49109ebfdd042d61cd1f0dd994
Markoz Pena <markdrew53@gmail.com>
2023-03-09 14:01:14 -0600

    refactor: 💡 change the altText image

--- src/components/commons/Header/index.tsx
+++ src/components/commons/Header/index.tsx
@@ -5,7 +5,7 @@ import './header.scss';
 
 const Header = () => (
   <header className="containerr">
-    <img src={logo} className="logo-app" alt="logo de rick and mort" />
+    <img src={logo} className="logo-app" alt="logo de rick and morty" />
     <h1 className="label">Juego de memoria</h1>
   </header>
 );
