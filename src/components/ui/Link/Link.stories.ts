bd5aaf1d49079c5f3bc8fa69f541952838572595
Markoz Pena <markdrew53@gmail.com>
2023-03-18 18:27:51 -0600

    feat: 🎸 create story for Link component

---
+++ src/components/ui/Link/Link.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Link from './Link';
+
+const meta: Meta<typeof Link> = {
+  title: 'Link',
+  component: Link,
+};
+
+export default meta;
+type Story = StoryObj<typeof Link>;
+
+export const Primary: Story = {
+  args: {
+    type: 'primary',
+    path: '/',
+    label: 'Jugar',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    type: 'secondary',
+    path: '/juego',
+    label: 'Jugar',
+  },
+};
